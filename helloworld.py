092a5cfc72ce54381caea3b5449cd1bcefc33c94
Lpint02 <leonardopinterpe@gmail.com>
2025-09-28 15:40:29 +0200

    first commit in new branch 'nuova-funzionalita'

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,7 @@
 name = input("What is your name? ")
 print(f"Hello {name} how are you?")
+mood = input("Come stai? (bene/male) ")
+if mood.lower() == "bene":
+    print(f"Mi fa piacere {name}!")
+else:
+    print(f"Mi dispiace {name}.")
